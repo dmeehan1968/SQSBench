6581c41e79115bc46250be1479fa3e4f59a95eec
Dave Meehan <dave_meehan@replicated.co.uk>
2024-08-30 19:00:21 +0100

    Outline stack

---
+++ app/SqsBench.ts
@@ -0,0 +1,19 @@
+import { Stack } from "aws-cdk-lib"
+import { Construct } from "constructs"
+
+interface Props {
+
+}
+
+export class SqsBench extends Stack {
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id)
+
+    // add tests
+
+    // add producer
+
+    // add dashboard
+
+  }
+}
--- app/index.ts
+++ app/index.ts
@@ -1,16 +1,6 @@
-import { Construct } from "constructs"
-import { App, Stack, Stage } from "aws-cdk-lib"
-
-export class MyConstruct extends Construct {
-    constructor(scope: Construct, id: string) {
-        super(scope, id)
-    }
-}
+import { App } from "aws-cdk-lib"
+import { SqsBench } from "./SqsBench"
 
 const app = new App()
 
-const stage = new Stage(app, 'MyStage')
-
-const stack = new Stack(stage, 'MyStack')
-
-const myConstruct = new MyConstruct(stack, 'MyConstruct')
+new SqsBench(app, 'SqsBench', {})
